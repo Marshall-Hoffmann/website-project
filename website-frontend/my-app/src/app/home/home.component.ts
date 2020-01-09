01fed0fc732ca1567677448a876912cc511316e9
Marshall Hoffmann <Hoff0899@gmail.com>
2020-01-09 17:53:26 -0600

    minor UI updates and remove unused components

--- website-frontend/my-app/src/app/home/home.component.ts
+++ website-frontend/my-app/src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 // import { SignedIn } from '../navbar/navbar.component';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { LoginService } from '../login.service';
+import { User } from '../shared/models/user';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +11,11 @@ import { NavbarComponent } from '../navbar/navbar.component';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService:LoginService) {
+    this.loginService.currentUser.subscribe(user => this.user = user);
+  }
+
+  user:User;
 
   ngOnInit() {
   }
